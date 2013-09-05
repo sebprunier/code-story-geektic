d8002d34fe19a015d49495cc71e8b0ef057d3f35
sebprunier <sebastien.prunier@gmail.com>
2013-09-05 23:31:21 +0200

    Add node.js scripts to populate mongo database

---
+++ scripts/populate.js
@@ -0,0 +1,54 @@
+'use strict';
+// require
+var MongoClient = require('mongodb').MongoClient,
+    async = require('async');
+
+// mongodb uri
+var uri = require('./conf/conf').MONGO_URL;
+
+// initial data
+var geeks = require('../web/geeks.json');
+
+var exitWithError = function(err) {
+    console.log('Something went wrong !');
+    console.log(err);
+    process.exit(1);
+};
+
+MongoClient.connect(uri, function(err, db) {
+    if (err) {
+        exitWithError(err);
+    }
+    db.collection('geeks', function(err, collection) {
+        if (err) {
+            exitWithError(err);
+        }
+        async.series(
+            [
+                // 1- remove the collection (if existing)
+                function(callback) {
+                    collection.remove({}, function(err, removed){
+                        callback(err, removed + " geek(s) removed !");
+                    });
+                },
+
+                // 2- insert geeks !
+                function(callback) {
+                    collection.insert(geeks, {safe : true},	function(err, result) {
+                        callback(err, result.length + " geek(s) inserted !");
+                    });
+                }
+            ],
+            // final callback function
+            function(err, results) {
+                if (err) {
+                    exitWithError(err);
+                }
+                results.map(function(result) {
+                    console.log(result);
+                });
+                process.exit(0);
+            }
+        );
+    });
+});
