2bbc2dc5e21dd1343774b4b0b65c6240012cb430
sebprunier <sebastien.prunier@gmail.com>
2013-09-16 14:33:38 +0200

    Add script to add geodata to the geeks collection

---
+++ scripts/src/addgeodata.js
@@ -0,0 +1,61 @@
+'use strict';
+// require
+var MongoClient = require('mongodb').MongoClient,
+    gm = require('googlemaps'),
+    async = require('async');
+
+// mongodb uri
+var uri = require('../conf/conf').MONGO_URL;
+
+var exitWithError = function (err) {
+    console.log('Something went wrong !');
+    console.log(err);
+    process.exit(1);
+};
+
+var geeksCollection;
+
+var addGeoData = function (geek, callback) {
+    if (geek.ville && geek.ville !== '') {
+        gm.geocode(geek.ville, function (err, cities) {
+            if (err) {
+                exitWithError(err);
+            }
+            if (cities.results && cities.results.length > 0) {
+                var geodata = cities.results[0];
+                geeksCollection.update({'_id': geek._id}, { $set: {'location': geodata.geometry.location}}, { upsert: true }, function (err, data) {
+                    if (err) {
+                        exitWithError(err);
+                    }
+                    console.log('Geek %s updated !', geek.nom);
+                    callback();
+                });
+            } else {
+                callback();
+            }
+        });
+    } else {
+        callback();
+    }
+};
+
+MongoClient.connect(uri, function (err, db) {
+    if (err) {
+        exitWithError(err);
+    }
+    db.collection('geeks', function (err, collection) {
+        if (err) {
+            exitWithError(err);
+        }
+        geeksCollection = collection;
+        geeksCollection.find().toArray(function (err, geeks) {
+            async.eachSeries(geeks, addGeoData, function (err) {
+                if (err) {
+                    exitWithError(err);
+                }
+                console.log('Done !');
+                process.exit(0);
+            });
+        });
+    });
+});
